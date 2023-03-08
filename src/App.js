b40fb1f4cff9bf85621793921c9b38289b58d4cb
avr87 <annavanruiten@gmail.com>
2023-03-08 17:46:35 +0000

    added the git-hub repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is created by Anna van Ruiten and is {" "}
+        <a
+          href="https://luminous-mousse-9fab79.netlify.app"
+          target="_blank"
+        >open-sourced on Github.</a>
+      </footer>
+    </div>
+  );
 }
